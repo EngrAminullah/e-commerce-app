4ad73c0b2d4c5a070e2732bc5632a9a270f2130a
Muhammad Arsalan <amin93pk@gmail.com>
2022-02-17 18:57:14 +0500

    create product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { NavLink } from "react-router-dom";
+import Skeleton from "react-loading-skeleton";
 
 const Product = () => {
   const { id } = useParams();
@@ -18,7 +19,22 @@ const Product = () => {
   }, []);
 
   const Loading = () => {
-    return <>Loading....</>;
+    return (
+      <>
+        <div className="col-md-6">
+          <Skeleton height={400} />
+        </div>
+        <div className="col-md-6" style={{ lineHeight: 2 }}>
+          <Skeleton height={50} width={300} />
+          <Skeleton height={75} />
+          <Skeleton height={25} width={150} />
+          <Skeleton height={50} />
+          <Skeleton height={150} />
+          <Skeleton height={50} width={100} />
+          <Skeleton height={50} width={100} style={{ marginLeft: 6 }} />
+        </div>
+      </>
+    );
   };
 
   const ShowProduct = () => {
@@ -31,22 +47,22 @@ const Product = () => {
             height="400px"
             width="400px"
           />
-          <div className="col-md-6">
-            <h4 className="text-uppercase text-black-50">{product.catagory}</h4>
-            <h1 className="display-5">{product.title}</h1>
-            <p className="lead fw-bolder">
-              Rating{product.rating && product.rating.rate}
-              <i className="fa fa-star"></i>
-            </p>
-            <h3 className="display-6 fw-bold my-4">$ {product.price}</h3>
-            <p className="lead">{product.description}</p>
-            <button className="btn btn-outline-dark px-4 py-2">
-              Add To Cart
-            </button>
-            <NavLink to="/cart" className="btn btn-dark ms-2 px-3 py-2">
-              Go To Cart
-            </NavLink>
-          </div>
+        </div>
+        <div className="col-md-6">
+          <h4 className="text-uppercase text-black-50">{product.catagory}</h4>
+          <h1 className="display-5">{product.title}</h1>
+          <p className="lead fw-bolder">
+            Rating{product.rating && product.rating.rate}
+            <i className="fa fa-star"></i>
+          </p>
+          <h3 className="display-6 fw-bold my-4">$ {product.price}</h3>
+          <p className="lead">{product.description}</p>
+          <button className="btn btn-outline-dark px-4 py-2">
+            Add To Cart
+          </button>
+          <NavLink to="/cart" className="btn btn-dark ms-2 px-3 py-2">
+            Go To Cart
+          </NavLink>
         </div>
       </>
     );
@@ -54,10 +70,8 @@ const Product = () => {
 
   return (
     <div>
-      <div className="container py-5 ">
-        <div className="row py-5">
-          {loading ? <Loading /> : <ShowProduct />}
-        </div>
+      <div className="container ">
+        <div className="row ">{loading ? <Loading /> : <ShowProduct />}</div>
       </div>
     </div>
   );
